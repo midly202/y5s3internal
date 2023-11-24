c1fbe00c866cdb20f0c44bb72c78a6d5b5d28c4f
midly202 <sakuraromako@gmail.com>
2023-11-24 13:03:07 +0100

    Stable build V1
    Features:
    - Firerate
    - Ammo
    - Damage
    - Recoil

--- y5s3internal/main.cpp
+++ y5s3internal/main.cpp
@@ -2,9 +2,13 @@
 // uintptr_t*	=	8 bytes
 // float*		=	float
 
+// Localplayer = ([[[R6GPM + 0x88]] + 0x30]) -0x6F
+// rewrite all pointers to one line
+// find the pointer for current weapon held, so it's possible to check if a weapon is held, and only THEN execute recoil / ammo 
 
 
 #define WIN_32_LEAN_AND_MEAN
+#include <iostream>
 #include <Windows.h>
 #include <cstdint>
 #include <thread>
@@ -16,41 +20,60 @@ namespace offset
 	constexpr std::ptrdiff_t dwFireRate = 0x0;
 }
 
-
-
 // hack fn
 void injected_thread(HMODULE instance) noexcept
 {
+    AllocConsole();
+    FILE* f = new FILE;
+    freopen_s(&f, "CONOUT$", "w", stdout);
 
-	uintptr_t* pBaseAddress = (uintptr_t*)GetModuleHandleA("RainbowSix.exe");
+    std::cout << "I am the alpha and the omega \n";
 
-	while (!GetAsyncKeyState(VK_INSERT))
-	{
-		Sleep(1);
+    uintptr_t* pBaseAddress = (uintptr_t*)GetModuleHandleA("RainbowSix.exe");
+    int count = 1;
 
-		uintptr_t* R6GamerProfileManager = reinterpret_cast<uintptr_t*>((uintptr_t)pBaseAddress + 0x5E32C50);
-		uintptr_t* intermediate1 = reinterpret_cast<uintptr_t*>(*R6GamerProfileManager + 0x88);
-		uintptr_t* intermediate2 = reinterpret_cast<uintptr_t*>(*intermediate1 + 0x0);
-		uintptr_t* intermediate3 = reinterpret_cast<uintptr_t*>(*intermediate2 + 0x30);
-		uintptr_t* intermediate4 = reinterpret_cast<uintptr_t*>(*intermediate3 + 0x21);
-		uintptr_t* intermediate5 = reinterpret_cast<uintptr_t*>(*intermediate4 + 0x70);
-		uint32_t* ammo_magazine = reinterpret_cast<uint32_t*>(*intermediate5 + 0x124);
+    while (!GetAsyncKeyState(VK_INSERT))
+    {
+        Sleep(10);
 
-		if (!ammo_magazine)
-			continue;
+        __try 
+        {
+            uintptr_t* R6GamerProfileManager = reinterpret_cast<uintptr_t*>((uintptr_t)pBaseAddress + 0x5E32C50);
+            uintptr_t* intermediate1 = reinterpret_cast<uintptr_t*>(*R6GamerProfileManager + 0x88);
+            uintptr_t* intermediate2 = reinterpret_cast<uintptr_t*>(*intermediate1 + 0x0);
+            uintptr_t* intermediate3 = reinterpret_cast<uintptr_t*>(*intermediate2 + 0x30);
+            uintptr_t* intermediate4 = reinterpret_cast<uintptr_t*>(*intermediate3 + 0x21);
+            uintptr_t* intermediate5 = reinterpret_cast<uintptr_t*>(*intermediate4 + 0x70);
+            uintptr_t* intermediate6 = reinterpret_cast<uintptr_t*>(*intermediate5 + 0x108);
+            uintptr_t* intermediate7 = reinterpret_cast<uintptr_t*>(*intermediate6 + 0x0);
+            uintptr_t* intermediate8 = reinterpret_cast<uintptr_t*>(*intermediate7 + 0x150);
+            uintptr_t* intermediate9 = reinterpret_cast<uintptr_t*>(*intermediate8 + 0x0);
+            uintptr_t* intermediate10 = reinterpret_cast<uintptr_t*>(*intermediate7 + 0x148);
+            uintptr_t* intermediate11 = reinterpret_cast<uintptr_t*>(*intermediate10 + 0x0);
 
-		__try
-		{
-			*ammo_magazine = 999;
-		}
-		__except(EXCEPTION_EXECUTE_HANDLER)
-		{
-
-		}
-	}
+            uintptr_t* gun_recoil = reinterpret_cast<uintptr_t*>(*intermediate7 + 0x140);
+            uint32_t* gun_firerate = reinterpret_cast<uint32_t*>(*intermediate9 + 0x3C);
+            uint32_t* gun_ammo = reinterpret_cast<uint32_t*>(*intermediate5 + 0x124);
+            uint32_t* gun_damage = reinterpret_cast<uint32_t*>(*intermediate11 + 0x6C);
 
 
-	FreeLibraryAndExitThread(instance, 0);
+            *gun_recoil = 0;
+            *gun_firerate = 9999;
+            *gun_ammo = 1000;
+            *gun_damage = 9999;
+            std::cout << "dereference succesful     " << count << "\n";
+            count++;
+        }
+
+        __except (EXCEPTION_EXECUTE_HANDLER)
+        {
+            std::cout << "dereference failed        " << count << "\n";
+            count++;
+        }
+    }
+    fclose(f);
+    FreeConsole();
+    FreeLibraryAndExitThread(instance, 0);
 }
 
 
